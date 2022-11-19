e56c23e4d3469531efca9f0989755a26d7292213
Aman Choudhri <amanchoudhri@gmail.com>
2022-11-18 23:45:23 -0500

    Update fit.py to work with mixed-membership models

--- src/fit.py
+++ src/fit.py
@@ -10,6 +10,7 @@ import time
 import json
 
 from pathlib import Path
+from typing import Union
 
 import numpy as np
 import arviz as az
@@ -18,22 +19,27 @@ from scipy.special import logsumexp
 from cmdstanpy import CmdStanModel, CmdStanVB
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+# logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
 RANDOM_SEED = 2022
 rng = np.random.default_rng(RANDOM_SEED)
 
-DEFAULT_DATA_PATH = Path('data') / 'data_splits_projected.npz'
+DEFAULT_DATA_PATH = Path('data') / 'concatenated.npz'
 DEFAULT_NUM_COMPONENTS = 5
 
-def hyperparams(K, D, model_type='diagonal_mixture'):
+def hyperparams(K, D) -> dict[str, Union[np.ndarray, int]]:
     """
     Return a set of sensible hyperparameters given the number of
     components K and the embedding dimension D.
+
+    Specifically, return a dict with the following entries:
+        alpha:   shape param for exchangeable Dirichlet prior on theta
+        mu:      prior means for beta
+        lambda:  prior variance for beta
+        gamma:   scale for half-Normal prior on sigma
     """
-    if model_type != 'diagonal_mixture':
-        raise NotImplementedError('Models other than diagonal mixture not yet available!')
     return {
         'alpha': np.ones(K), # shape param for exchangeable Dirichlet prior on theta
         'mu': np.zeros((K, D)),     # prior means for beta
@@ -41,11 +47,6 @@ def hyperparams(K, D, model_type='diagonal_mixture'):
         'gamma': 5,  # scale for half-Normal prior on sigma
     }
 
-
-def load_data(path):
-    d = np.load(path)
-    X_train, X_test = d['X_train'], d['X_test']
-    return X_train, X_test
 
 def get_args():
     # defaults should be the output of default_hyperparams
@@ -64,13 +65,14 @@ def get_args():
     return parser.parse_args()
 
 def fit_meanfield(
-    X_train,
-    X_test,
+    data: dict[str, np.ndarray],
     K: int,
     model_file='diagonal_mixture.stan',
+    num_train=None,
+    num_test=None
     ) -> CmdStanVB:
     """
-    Create a mixture model with the following hyperparameters:
+    Create a mixture or mixed-membership model with the following hyperparameters:
 
         K:  number of vocalization types
         alpha: shape param for exchangeable Dirichlet prior on theta
@@ -78,9 +80,22 @@ def fit_meanfield(
         lambda: shapes for Gaussian prior on beta
         gamma: scale for half-Normal prior on the mixture variances, sigma
 
-    NOTE: Expects X_train to be of shape (num_cohorts, num_observations, embedding_dimension).
+    NOTE: Expects X_train to be of shape (num_observations, embedding_dimension).
     """
-    logger.info(f'Creating model on data of shape {X_train.shape}.')
+    X_train, X_test = data['X_train'], data['X_test']
+
+    logger.info(
+        f'Creating model on input data of shape {X_train.shape} '
+        f'with held-out data of shape {X_test.shape}'
+        )
+
+    # restrict the number of training and test observations if necessary
+    if num_train:
+        X_train = X_train[:num_train]
+        logger.info(f'Truncating train vocalizations to {num_train}')
+    if num_test:
+        X_test = X_test[:num_test]
+        logger.info(f'Truncating test vocalizations to {num_test}')
 
     N = X_train.shape[0]  # number of observations
     D = X_train.shape[1]  # embedding dimension
@@ -89,17 +104,62 @@ def fit_meanfield(
 
     model = CmdStanModel(stan_file=model_file)
 
-    data = {
+    model_params = {
         'K': K,
         'D': D,
         'N': N,
         'X': X_train,
         'N_out': N_out,
         'X_out': X_test,
-        **hyperparams(K, D),
+        **hyperparams(K, D)
     }
 
-    return model.variational(data=data)
+    if 'mixed_membership' in model_file:
+        og_train_lens, og_test_lens = data['train_lens'], data['test_lens']
+        logger.info('Building mixed-membership model.')
+        
+        logger.info(
+            f'Original number of vox per family in train set: {og_train_lens}. '
+            f'Original # in test set: {og_test_lens}'
+            )
+
+        # adjust train and test lengths if user truncated data
+        def __get_lens(N_vocalizations, og_lens) -> list[int]:
+            lens = []
+            total = 0
+            for l in og_lens:
+                if total + l <= N_vocalizations:
+                    lens.append(l)
+                    total += l
+                elif total + l > N_vocalizations:
+                    num_able_to_include = max(N_vocalizations - total, 0)
+                    lens.append(num_able_to_include)
+                    break
+            return lens
+
+        train_lens = __get_lens(N, og_train_lens)
+        test_lens = __get_lens(N_out, og_test_lens)
+
+        logger.info(
+            f'After optional truncations, number of vox per family in train set: {train_lens}. '
+            f'In test set: {test_lens}'
+            )
+
+        # make sure the same number of families are in both sets
+        if len(train_lens) != len(test_lens):
+            raise ValueError(
+                f'Data truncations resulted in differing numbers of families '
+                f'in the train ({len(train_lens)}) and test ({len(test_lens)}) sets.'
+                )
+
+        model_params['lens'] = train_lens
+        model_params['lens_out'] = test_lens
+        model_params['F'] = len(train_lens) # number of families
+    else:
+        logger.info('Building mixture model.')
+
+    logger.info(f'Running ADVI...')
+    return model.variational(data=model_params)
 
 
 if __name__ == '__main__':
@@ -115,13 +175,14 @@ if __name__ == '__main__':
     logger.addHandler(logging.FileHandler(outdir / 'fit.log'))
     logger.info(f'Arguments recieved: {args}')
 
-    X_train, X_test = load_data(path=args.data)
+    # load the data
     logger.info(f'Data from path: {args.data}')
+    data = np.load(args.data)
 
-    logger.info(f'Running ADVI...')
 
     # load model
     filename = f'{args.model_name}.stan' if '.stan' not in args.model_name else args.model_name
+    logger.info(f'Attempting to locate model architecture with filename: {filename}')
     models_dir_attempt = Path('models') / filename
     cwd_attempt = Path(filename)
     # check the models subdirectory
@@ -133,16 +194,13 @@ if __name__ == '__main__':
     else:
         raise ValueError(f'Could not file model {args.model_name}!')
 
-    if args.num_train:
-        X_train = X_train[:args.num_train]
-    if args.num_test:
-        X_test = X_test[:args.num_test]
 
     fit = fit_meanfield(
-        X_train,
-        X_test,
+        data,
         K=args.K,
-        model_file=model_file
+        model_file=model_file,
+        num_train=args.num_train,
+        num_test=args.num_test,
         )
 
     fit_az = az.from_cmdstan(fit.runset.csv_files)
--- src/run_models.sh
+++ src/run_models.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #SBATCH -p ccn
 #SBATCH -N 1
-# #SBATCH --array=4,5,8,10,12,15,18,20
+#SBATCH --array=4,5,8,10,12,15,18,20
 #SBATCH --time=12:00:00
 #SBATCH -o logs/run_models_%j.log
 
 MODEL=$1
 
 case $MODEL in
-    diagonal_mixture|mixture) echo "Selected model ${MODEL}";;
-    *) echo "Invalid model type ${MODEL}!" && exit;
+    diagonal_mixture|mixture|diagonal_mixed_membership|mixed_membership)
+        echo "Selected model ${MODEL}"
+        ;;
+    *)
+        echo "Invalid model type ${MODEL}!"
+        exit
+        ;;
 esac
 
 OUTDIR=$2
@@ -21,7 +26,7 @@ fi
 DATA=$3
 
 if [[ -z $DATA ]]; then
-    DATA=data/data_splits_projected.npz
+    DATA=data/concatenated.npz
 fi
 
 python fit.py $OUTDIR $MODEL --K $SLURM_ARRAY_TASK_ID --data $DATA
